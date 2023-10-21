aedf08ec131727263d5950be29772912925e23cd
Pramod Kumar <anchaljaiswal1341@gmail.com>
2023-10-21 13:32:11 +0530

    removed unwanted comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { Button, Form } from "react-bootstrap";
 import "./index.css";
 import { useCallback, useEffect, useRef, useState } from "react";
-// import { AxiosContext } from "react-axios/lib/components/AxiosProvider";
 import axios from "axios";
 
 const API_URL = "https://api.unsplash.com/search/photos";
 const IMAGES_PER_PAGE = 24;
-//we use axios to call the api and without api key , the api will not work
 
 function App() {
-  // console.log('key',process.env.REACT_APP_API_KEY);
   const searchInput = useRef(null);
   const [images, setImages] = useState([]);
   const [page, setPage] = useState(1);
@@ -20,7 +17,6 @@ function App() {
       const { data } = await axios.get(
         `${API_URL}?query=${searchInput.current.value}&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${process.env.REACT_APP_API_KEY}`
       );
-      // console.log('result',result .data)
       setImages(data.results);
       setTotalPages(data.total_pages);
     } catch (error) {
@@ -38,21 +34,15 @@ function App() {
   };
 
   const handleSearch = (event) => {
-    event.preventDefault(); //to avoid page refresh everytime
-    // console.log(searchInput.current.value);
+    event.preventDefault();
     resetSearch();
   };
 
-  // const handleSelection = (selection)=>{
-  //   searchInput.current.value = selection;
-  //   fetchImages;
-  // }
   return (
     <div className="container">
       <h1 className="title">Image Search</h1>
       <div className="search-section">
         <Form onSubmit={handleSearch}>
-          {/* //here we used inside form bcz we don't have any submit button to do enter so to automatically do it, we used that         */}
           <Form.Control
             type="search"
             placeholder="Type something to search..."
@@ -62,22 +52,18 @@ function App() {
         </Form>
       </div>
       <div className="images">
-  {images.map((image) => (
-    <div key={image.id} className="image">
-      <img src={image.urls.small} alt={image.alt_description} />
-      <div className="info-container">
-        <div className="username">{image.user.username}</div>
-        <div className="likes">
-          <i className="fa fa-thumbs-up"></i> {image.likes}
-        </div>
+        {images.map((image) => (
+          <div key={image.id} className="image">
+            <img src={image.urls.small} alt={image.alt_description} />
+            <div className="info-container">
+              <div className="username">{image.user.username}</div>
+              <div className="likes">
+                <i className="fa fa-thumbs-up"></i> {image.likes}
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
-    </div>
-  ))}
-</div>
-
-
-
-
 
       <div className="buttons">
         {page > 1 && (
